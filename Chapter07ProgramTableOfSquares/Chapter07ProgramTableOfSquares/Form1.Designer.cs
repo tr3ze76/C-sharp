d70803b73e858492c4444982c65c7ddd10fdd97c
Alberto76 <58238275+tr3ze76@users.noreply.github.com>
2020-01-20 19:11:29 -0500

    Add files via upload

---
+++ Chapter07ProgramTableOfSquares/Chapter07ProgramTableOfSquares/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Chapter07ProgramTableOfSquares
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStart = new System.Windows.Forms.Label();
+            this.lblEnd = new System.Windows.Forms.Label();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblStart
+            // 
+            this.lblStart.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.lblStart.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblStart.Location = new System.Drawing.Point(28, 34);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(150, 23);
+            this.lblStart.TabIndex = 0;
+            this.lblStart.Text = "Starting table value";
+            this.lblStart.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblEnd
+            // 
+            this.lblEnd.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.lblEnd.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblEnd.Location = new System.Drawing.Point(28, 67);
+            this.lblEnd.Name = "lblEnd";
+            this.lblEnd.Size = new System.Drawing.Size(150, 23);
+            this.lblEnd.TabIndex = 1;
+            this.lblEnd.Text = "Ending table value";
+            this.lblEnd.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(207, 34);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(100, 20);
+            this.txtStart.TabIndex = 2;
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(207, 69);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(100, 20);
+            this.txtEnd.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(28, 117);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Ca&lculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(125, 117);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clea&r";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(232, 117);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(28, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 23);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Number";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(165, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Number Squared";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.Location = new System.Drawing.Point(28, 192);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(279, 225);
+            this.lstOutput.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 450);
+            this.Controls.Add(this.lstOutput);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.lblEnd);
+            this.Controls.Add(this.lblStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.Label lblEnd;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lstOutput;
+    }
+}
+
---
+++ Chapter07ProgramTableOfSquares/Chapter07ProgramTableOfSquares/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter07ProgramTableOfSquares
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+     
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            txtStart.Clear();
+            txtEnd.Clear();
+            lstOutput.Items.Clear();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            bool flag;
+            int i;
+            int j;
+            int start;
+            int end;
+            int square;
+            int nextOddInteger;
+            string buff;
+
+            flag = int.TryParse(txtStart.Text, out start);
+            if (flag == false)
+            {
+                MessageBox.Show("Numeric data only", "Input Error");
+                txtEnd.Focus();
+                return;
+            }
+
+            flag = int.TryParse(txtEnd.Text, out end);
+            if (flag == false)
+            {
+                MessageBox.Show("Numeric data only", "Input Error");
+                txtStart.Focus();
+                return;
+            }
+
+
+            square = 0;
+               
+                for (i = start; i <= end; i++)
+                {
+                    {
+                        buff = string.Format("{0,5}{1,20}", i, i * i);
+                        lstOutput.Items.Add(buff);
+                    }
+                }
+                
+
+                for (i = start; i <= end; i++)
+                {
+                    nextOddInteger = 1;
+                    square = 0;
+                    
+                    for (j = 0; j < i; j++)
+                    {
+                        square += nextOddInteger;
+                        nextOddInteger += 2;
+                    }
+                }
+
+                buff = string.Format("{0,5}{1,20}", i, square);
+                lstOutput.Items.Add(buff);
+
+            
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
