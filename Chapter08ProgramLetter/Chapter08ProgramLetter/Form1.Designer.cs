a0cf52fca5ecd4a28fce7c27bdf80a89d6b6a683
Alberto76 <58238275+tr3ze76@users.noreply.github.com>
2020-01-23 19:27:11 -0500

    Add files via upload

---
+++ Chapter08ProgramLetter/Chapter08ProgramLetter/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Chapter08ProgramLetter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.Label();
+            this.Lenght = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.txtInput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtInput.Location = new System.Drawing.Point(12, 23);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(267, 23);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.Text = "Enter text";
+            this.txtInput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Lenght
+            // 
+            this.Lenght.Location = new System.Drawing.Point(12, 49);
+            this.Lenght.Multiline = true;
+            this.Lenght.Name = "Lenght";
+            this.Lenght.Size = new System.Drawing.Size(267, 181);
+            this.Lenght.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 246);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Ca&lculte";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(204, 246);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(12, 290);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Letter";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(179, 290);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Count";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.Location = new System.Drawing.Point(12, 332);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(267, 186);
+            this.lstOutput.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 553);
+            this.Controls.Add(this.lstOutput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.Lenght);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtInput;
+        private System.Windows.Forms.TextBox Lenght;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstOutput;
+    }
+}
+
---
+++ Chapter08ProgramLetter/Chapter08ProgramLetter/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter08ProgramLetter
+{
+    public partial class Form1 : Form
+    {
+        
+
+        private const int MAXLETTERS = 26;
+        private const int MAXCHARS = MAXLETTERS - 1;
+        private const int LETTERA = 65;
+    
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            char oneLetter;
+            int index;
+            int i;
+            int length;
+            int[] count = new int[MAXLETTERS];
+            string input;
+            string buff;
+
+
+            length = txtInput.Text.Length;
+            if (length == 0)
+            {
+                MessageBox.Show("you need to enter some text.", "Missing Input");
+                txtInput.Focus();
+                return;
+            }
+            input = txtInput.Text;            
+            input = input.ToUpper();
+            for (i = 0; i < input.Length; i++)
+            {
+                oneLetter = input[i];
+                index = oneLetter - LETTERA;
+                if (index < 0 || index > MAXCHARS)
+                    continue;
+                count[index]++;
+            }
+            for (i = 0; i < MAXLETTERS; i++)
+            {
+                buff = string.Format("{0,4} {1,20} [{2}]", (char)(i + LETTERA), "", count[i]);
+                lstOutput.Items.Add(buff);
+            }
+
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
