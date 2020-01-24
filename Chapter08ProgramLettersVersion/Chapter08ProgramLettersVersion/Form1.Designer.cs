1e577a28a946a76f0485da56a9d7f4c5558b7cf5
Alberto76 <58238275+tr3ze76@users.noreply.github.com>
2020-01-23 19:23:05 -0500

    Add files via upload

---
+++ Chapter08ProgramLettersVersion/Chapter08ProgramLettersVersion/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Chapter08ProgramLettersVersion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("ListViewGroup", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup2 = new System.Windows.Forms.ListViewGroup("ListViewGroup", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+            this.txtInput = new System.Windows.Forms.ListView();
+            this.lsvOutput = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.HideSelection = false;
+            this.txtInput.Location = new System.Drawing.Point(24, 61);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(282, 195);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.UseCompatibleStateImageBehavior = false;
+            this.txtInput.SelectedIndexChanged += new System.EventHandler(this.TxtInput_SelectedIndexChanged);
+            // 
+            // lsvOutput
+            // 
+            this.lsvOutput.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            listViewGroup1.Header = "ListViewGroup";
+            listViewGroup1.Name = "listViewGroup1";
+            listViewGroup2.Header = "ListViewGroup";
+            listViewGroup2.Name = "listViewGroup2";
+            this.lsvOutput.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
+            listViewGroup1,
+            listViewGroup2});
+            this.lsvOutput.HideSelection = false;
+            this.lsvOutput.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+            this.lsvOutput.Location = new System.Drawing.Point(24, 326);
+            this.lsvOutput.Name = "lsvOutput";
+            this.lsvOutput.Size = new System.Drawing.Size(282, 148);
+            this.lsvOutput.TabIndex = 1;
+            this.lsvOutput.UseCompatibleStateImageBehavior = false;
+            this.lsvOutput.View = System.Windows.Forms.View.List;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(24, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(282, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1Enter Text";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(24, 280);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(94, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(215, 280);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(91, 23);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 502);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lsvOutput);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView txtInput;
+        private System.Windows.Forms.ListView lsvOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ Chapter08ProgramLettersVersion/Chapter08ProgramLettersVersion/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter08ProgramLettersVersion
+{
+    public partial class Form1 : Form
+    {
+
+
+        private const int MAXLETTERS = 26;
+        private const int MAXCHARS = MAXLETTERS - 1;
+        private const int LETTERA = 65;
+       
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            char oneLetter;
+            int index;
+            int i;
+            int length;
+            int[] count = new int[MAXLETTERS];
+            string input;
+           // string buff;
+
+
+            length = txtInput.Text.Length;
+            if (length == 0)
+            {
+                MessageBox.Show("you need to enter some text.", "Missing Input");
+                txtInput.Focus();
+                return;
+            }
+            input = txtInput.Text;
+            input = input.ToUpper();
+            for (i = 0; i < input.Length; i++)
+            {
+                oneLetter = input[i];
+                index = oneLetter - LETTERA;
+                if (index < 0 || index > MAXCHARS)
+                    continue;
+                count[index]++;
+            }
+            ListViewItem which;
+            for (i = 0; i < MAXLETTERS; i++)
+            {
+                oneLetter = (char)(i + LETTERA);
+                which = new ListViewItem(oneLetter.ToString());
+                which.SubItems.Add(count[i].ToString());
+                lsvOutput.Items.Add(which);
+            }
+        }
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void TxtInput_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
