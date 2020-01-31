8e423676b154792b0888121bb26dc5e5e762d8c7
Alberto76 <58238275+tr3ze76@users.noreply.github.com>
2020-01-30 19:27:12 -0500

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+
+            int result = 1;
+
+            Console.WriteLine("Hello Uber");
+            int[] arrInput = { 1, 2, 3, };
+            int[] arrOutput = new int[arrInput.Length];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (i != j)
+                    {
+                        Console.WriteLine(arrInput[j]);
+                        result = (arrInput[j] * result);
+                    }
+
+                }
+                arrOutput[i] = result;
+                result = 1;
+
+                Console.WriteLine(result);
+            }
+            for (int x = 0; x < 3; x++)
+
+
+
+                Console.ReadKey();
+
+
+
+
+
+        }
+
+    }
+}
